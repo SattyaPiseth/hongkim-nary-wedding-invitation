32130565c456f7327d50872e45ef42289af1d298
Sattya Piseth <sattya.piseth@technovage.io>
2025-11-07 14:25:09 +0700

    Auto update: 2025-11-07 14:25:07

--- src/App.jsx
+++ src/App.jsx
@@ -400,7 +400,7 @@ export default function App() {
 
       {/* Global route-transition overlay */}
         {isRouting && (
-        <div className="fixed inset-0 z-[80] grid place-items-center bg-black/10 backdrop-blur-[2px]">
+        <div className="fixed inset-0 z-80 grid place-items-center bg-black/10 backdrop-blur-[2px]">
           <span className="sr-only">Navigating…</span>
         </div>
       )}
